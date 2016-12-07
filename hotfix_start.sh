3d7a7a5b328965b08bd7af31a29b13ec6d8d86c1
rameshmanickam <ramesh.manickam@BDC7-L-5464X33.dir.svc.accenture.com>
2016-12-07 15:16:03 +0530

    f

--- hotfix_start.sh
+++ hotfix_start.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-#export PATH="${tools_repo}"/jenkins/gitflow:$PATH
+#export PATH="${jenkins_tools_repo}"/jenkins/gitflow:$PATH
 
-if [ -z "${TAG_NAME}" ] ; then
-	printf "\n*** TAG_NAME is blank. BRANCH WILL BE CREATED BASED ON THE LATEST OF MASTER****\n"
-fi 
+#if [ -z "${TAG_NAME}" ] ; then
+#	printf "\n*** TAG_NAME is blank. BRANCH WILL BE CREATED BASED ON THE LATEST OF MASTER****\n"
+#fi 
 
 git pull origin master
 git checkout master
-
+git checkout -b $BRANCH_NAME
 #git checkout "${ORIGIN_BRANCH}"
-git-flow init -f <<EOF
-master
-$ORIGIN_BRANCH
-feature/
-release/
-hotfix/
-EOF
+#git-flow init -f <<EOF
+#master
+#$ORIGIN_BRANCH
+#feature/
+#release/
+#hotfix/
+#EOF
 
-git-flow "${BRANCH_TYPE}" start "${BRANCH_NAME}" "${TAG_NAME}"<<EOF
-$BRANCH_NAME
-$SRC_BRANCH
-EOF
+#git-flow "${BRANCH_TYPE}" start "${BRANCH_NAME}" "${TAG_NAME}"<<EOF
+#$BRANCH_NAME
+#$SRC_BRANCH
+#EOF
 
 #checking if the last command was executed successfully = branch created successfully
 if [ $? -eq 0 ] ; then
@@ -28,7 +28,7 @@ if [ $? -eq 0 ] ; then
 	git add .
 	git commit -m "CREATING $BRANCH_TYPE: ${BRANCH_NAME} - WITH CONFIGURATION" || echo 'Commit failed. There is probably nothing to commit.'
 	git push -u "${REPO}" "${BRANCH_TYPE}/${BRANCH_NAME}"
-	#cd $WORKSPACE
+	cd $WORKSPACE
 	echo "done"
 else
 	printf "\n***HOTFIX BRANCH CREATION FAILED****\n"
